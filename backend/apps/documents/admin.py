88060df17c0433f495b549a2eaeea78ce9ee1096
Josué Santizo <jsantizosanchez@gmail.com>
2026-08-19 10:33:43 -0600

    Autonomo (#389)
    Co-authored-by: Pablo Mazariegos <mazariegospablo928@gmail.com>

--- backend/apps/documents/admin.py
+++ backend/apps/documents/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from apps.documents.models import DocumentTemplate, DocumentTemplateVersion
+from apps.documents.models import DocumentRecord, DocumentTemplate, DocumentTemplateVersion
 
 admin.site.register(DocumentTemplate)
 admin.site.register(DocumentTemplateVersion)
+admin.site.register(DocumentRecord)
